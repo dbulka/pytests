93331dae03c51a23e120e95c50639e1062020339
sergey ladutko <s.ladutko@pixelplex.io>
2019-10-29 12:35:53 +0300

    fix test

--- suites/HistoryApi/GetAccountHistoryOperations.py
+++ suites/HistoryApi/GetAccountHistoryOperations.py
@@ -25,6 +25,7 @@ class GetAccountHistoryOperations(BaseTest):
 
     def setup_suite(self):
         super().setup_suite()
+        self._connect_to_echopy_lib()
         lcc.set_step("Setup for {}".format(self.__class__.__name__))
         self.__database_api_identifier = self.get_identifier("database")
         self.__registration_api_identifier = self.get_identifier("registration")
@@ -36,6 +37,10 @@ class GetAccountHistoryOperations(BaseTest):
         self.echo_acc0 = self.get_account_id(self.accounts[0], self.__database_api_identifier,
                                              self.__registration_api_identifier)
         lcc.log_info("Echo account is '{}'".format(self.echo_acc0))
+
+    def teardown_suite(self):
+        self._disconnect_to_echopy_lib()
+        super().teardown_suite()
 
     @lcc.test("Simple work of method 'get_account_history_operations'")
     def method_main_check(self):
